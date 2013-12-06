9ac01e995764268fef71a14b4922b9040ed6ba5e
Emiliano Zilocchi <ezilocchi@gmail.com>
2013-12-06 11:26:32 -0300

    Set rake gem as dependency for install vim-janus

--- modules/vim/manifests/init.pp
+++ modules/vim/manifests/init.pp
@@ -3,11 +3,20 @@ class vim {
     ensure => installed
   }
 
-  #Ruby dependencies are defined as part of the puppet instalation process (see install.sh)
+  exec { 'gem-rake':
+    cwd     => "${home}",
+    command => '/usr/bin/gem install rake'
+  }
+
+  #Ruby interpreter is installed as part of the puppet instalation process (see install.sh)
   exec { 'vim-janus':
     cwd     => "${home}",
     command => '/usr/bin/curl -Lo- https://bit.ly/janus-bootstrap | bash',
     creates => "${home}/.vim/janus",
-    require => [Package['curl', 'vim', 'ack-grep', 'exuberant-ctags'], Class['git']]
+    require => [
+      Package['curl', 'vim', 'ack-grep', 'exuberant-ctags'],
+      Class['git'],
+      Exec['gem-rake']
+    ]
   }
 }

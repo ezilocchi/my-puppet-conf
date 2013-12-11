13a420c9a5808ded51e5e2684b88c4e32c18f535
Emiliano Zilocchi <ezilocchi@gmail.com>
2013-12-11 11:37:21 -0300

    Run git config unless it is already set

--- modules/git/manifests/init.pp
+++ modules/git/manifests/init.pp
@@ -5,14 +5,17 @@ class git {
 
   exec { '/usr/bin/git config --global push.default simple':
     environment => "HOME=${home}",
-    require     => [Package['git']]
+    require     => [Package['git']],
+    unless      => '/usr/bin/git config --list | grep "push.default=simple"'
   }
   exec { '/usr/bin/git config --global core.editor vim':
     environment => "HOME=${home}",
-    require     => [Package['git']]
+    require     => [Package['git']],
+    unless      => '/usr/bin/git config --list | grep "core.editor=vim"'
   }
   exec { '/usr/bin/git config --global color.ui true':
     environment => "HOME=${home}",
-    require     => [Package['git']]
+    require     => [Package['git']],
+    unless      => '/usr/bin/git config --list | grep "color.ui=true"'
   }
 }
